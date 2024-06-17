5ec09842952cf1f839706f7a0ebbc274d6c9ec4b
Gabriel Bg <105066266+Jhogab10@users.noreply.github.com>
2024-06-17 08:20:15 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,56 +6,108 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Impactos da Poluição do Ar: Desafios e Soluções para a Preservação Ambiental",
+        enunciado: "Você tem conhecimento sobre a poluição do ar?",
         alternativas: [
-            "Ok, vamos lá!",
-            "Isso é terrível!"
-        ]
-    },
-    {
-        enunciado: "Quais das seguintes opções são consideradas principais fontes de poluição do ar em áreas urbanas e rurais?",
-        alternativas: [
-            "Emissões veiculares",
-            "Indústrias"
-        ]
-    },
-    {
-        enunciado: "",
-        alternativas: [
-            "",
-            ""
+            {
+                texto: "sou leigo neste assunto!",
+                afirmacao: "afimação"
+            },
+            {
+                texto: "obtenho este conhecimento!",
+                afirmacao: "afirmação"
+            }
         ]
     },
     {
         enunciado: "",
         alternativas: [
-            "",
-            ""
+            {
+                texto: "Sim!!!",
+                afirmacao: "afirmação"
+            },
+            {
+                texto: "Não!!!",
+                afirmacao: "afirmação"
+            }
         ]
     },
     {
-        enunciado: "",
+        enunciado: "Realiza alguma ativida para a preservação do ambiente?",
         alternativas: [
-            "",
-            ""
+            {
+                texto: "SIM! REALIZO",
+                afirmacao: "afirmação"
+            },
+            {
+                texto: "NÃO! NÃO AJUDO",
+                afirmacao: "afirmação"
+            }
         ]
-    },Indústrias
+    },
+    {
+        enunciado: "Prejudica de alguma forma o meio ambiente?",
+        alternativas: [
+            {
+                texto: "SIM! Prejudico",
+                afirmacao: "afirmação"
+            },
+            {
+                texto: "NÃO! NÃo Prejudico",
+                afirmacao: "afirmação"
+            }
+        ]
+    },
+    {
+        enunciado: "Você acredita na melhora ambiental a curto prazo? ",
+        alternativas: [
+            {
+                texto: "SIM! temos capacidade",
+                afirmacao: "afirmação"
+            },
+            {
+                texto: "NÃO! nao temos condições",
+                afirmacao: "afirmação"
+            }
+        ]
+    },
 ];
+
 
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = "";
 
 function mostraPergunta() {
+    if (atual >= perguntas.length) {
+        mostraResultado();
+        return;
+    }
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
     mostraAlternativas();
 }
-function mostraAlternativas() {
+
+function mostraAlternativas(){
     for(const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
-        botaoAlternativas.textContent = alternativa;
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
         caixaAlternativas.appendChild(botaoAlternativas);
     }
+}
+
+function respostaSelecionada(opcaoSelecionada) {
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + " ";
+    atual++;
+    mostraPergunta();
+}
+
+function mostraResultado() {
+    caixaPerguntas.textContent = "Em 2049...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
 }
 
 mostraPergunta();
